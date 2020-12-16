84ab3d888776cd1404b4d675d6266448a00a4b2d
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-12-16 13:09:09 -0500

    Got Percolation Initialization Working:
    Got the init working for percolation, which virtual sites connected on the top and bottom. Also created a test suite for Percolation

--- week_0/assignments/percolation/Percolation.py
+++ week_0/assignments/percolation/Percolation.py
@@ -5,7 +5,14 @@ from week_0.union_find.OptimalUnionFind import OptimalUnionFind
 class Percolation(PercolationInterface):
 
     def __init__(self, n: int):
-        self.unionFind: OptimalUnionFind = OptimalUnionFind(n)
+        self.dimension: int = n
+        self.total_normal_sites: int = n ** 2
+        total_virtual_sites: int = 2
+        self.union_find: OptimalUnionFind = OptimalUnionFind(self.total_normal_sites + total_virtual_sites)
+        self.top_virtual_site: int = self.total_normal_sites
+        self.bottom_virtual_site: int = self.total_normal_sites + 1
+        self.__connect_virtual_site(self.top_virtual_site)
+        self.__connect_virtual_site(self.bottom_virtual_site)
 
     def open(self, row: int, col: int):
         pass
@@ -21,4 +28,17 @@ class Percolation(PercolationInterface):
 
     def percolates(self) -> bool:
         return False
+
+    def __connect_virtual_site(self, virtual_site: int):  # O(n*log(n))
+        sites_to_be_connected: list
+        if virtual_site == self.top_virtual_site:
+            sites_to_be_connected = [
+                site for site in range(0, self.dimension)
+            ]
+        else:
+            sites_to_be_connected = [
+                site for site in range(self.dimension * (self.dimension - 1), self.total_normal_sites)
+            ]
+        for site in sites_to_be_connected:
+            self.union_find.union(virtual_site, site)
 
---
+++ week_0/assignments/percolation/test_Percolation.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from week_0.assignments.percolation.Percolation import Percolation
+
+
+class TestPercolation(TestCase):
+
+    def test_that_all_sites_are_created(self):
+        n: int = 4
+        percolation: Percolation = Percolation(n)
+        expected: int = n**2 + 2
+        actual: int = len(percolation.union_find.roots)
+        self.assertEqual(expected, actual)
+
+    def test_open(self):
+        self.fail()
+
+    def test_is_open(self):
+        self.fail()
+
+    def test_is_full(self):
+        self.fail()
+
+    def test_number_of_open_sites(self):
+        self.fail()
+
+    def test_percolates(self):
+        self.fail()
