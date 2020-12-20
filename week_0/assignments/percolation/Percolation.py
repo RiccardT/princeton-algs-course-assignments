7a6a9f35caa59437ea948686e96a98247d36087a
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-12-20 13:32:04 -0500

    More Input Validation
    Kappa

--- week_0/assignments/percolation/Percolation.py
+++ week_0/assignments/percolation/Percolation.py
@@ -7,6 +7,7 @@ from week_0.union_find.OptimalUnionFind import OptimalUnionFind
 class Percolation(PercolationInterface):
 
     def __init__(self, n: int):
+        self.__validate_init_input(n)
         self.__n: int = n
         self.__total_normal_sites: int = n ** 2
         total_virtual_sites: int = 2
@@ -18,8 +19,26 @@ class Percolation(PercolationInterface):
         self.__opened: List[bool] = [False for _ in range(self.__total_normal_sites)]
         self.__number_of_open_sites: int = 0
 
+    @staticmethod
+    def __validate_init_input(n: int):
+        if n <= 0:
+            Exception(f'Input of n: {n} is invalid, n must be greater than 0')
+
+    def __connect_virtual_site(self, virtual_site: int):  # O(n*log(n))
+        sites_to_be_connected: List[int]
+        if virtual_site == self.__top_virtual_site:
+            sites_to_be_connected = [
+                site for site in range(0, self.__n)
+            ]
+        else:
+            sites_to_be_connected = [
+                site for site in range(self.__n * (self.__n - 1), self.__total_normal_sites)
+            ]
+        for site in sites_to_be_connected:
+            self.__union_find.union(virtual_site, site)
+
     def open(self, row: int, col: int):
-        self.__validate_inputs(row, col)
+        self.__validate_grid_inputs(row, col)
         u_f_node: int = self.__get_associated_u_f_node(row, col)
         self.__opened[u_f_node] = True
         neighbor_u_f_nodes: List[int] = self.__get_neighbor_u_f_nodes(u_f_node)
@@ -28,11 +47,11 @@ class Percolation(PercolationInterface):
                 self.__union_find.union(u_f_node, neighbor_node)
         self.__number_of_open_sites += 1
 
-    def __validate_inputs(self, row: int, col: int):
+    def __validate_grid_inputs(self, row: int, col: int):
         if row < 0 or row >= self.__n:
-            raise Exception(f'Input of row:{row} out of range, row must be between 0 and {self.__n}')
+            raise Exception(f'Input of row: {row} out of range, row must be between 0 and {self.__n}')
         elif col < 0 or col >= self.__n:
-            raise Exception(f'Input of col:{col} out of range, col must be between 0 and {self.__n}')
+            raise Exception(f'Input of col: {col} out of range, col must be between 0 and {self.__n}')
 
     def __get_associated_u_f_node(self, row: int, col: int) -> int:
         return row * self.__n + col
@@ -50,12 +69,12 @@ class Percolation(PercolationInterface):
         return actual_neighbors
 
     def is_open(self, row: int, col: int) -> bool:
-        self.__validate_inputs(row, col)
+        self.__validate_grid_inputs(row, col)
         u_f_node: int = self.__get_associated_u_f_node(row, col)
         return self.__opened[u_f_node]
 
     def is_full(self, row: int, col: int) -> bool:
-        self.__validate_inputs(row, col)
+        self.__validate_grid_inputs(row, col)
         u_f_node: int = self.__get_associated_u_f_node(row, col)
         return self.__opened[u_f_node] and self.__union_find.connected(self.__top_virtual_site, u_f_node)
 
@@ -64,17 +83,3 @@ class Percolation(PercolationInterface):
 
     def percolates(self) -> bool:
         return self.__union_find.connected(self.__top_virtual_site, self.__bottom_virtual_site)
-
-    def __connect_virtual_site(self, virtual_site: int):  # O(n*log(n))
-        sites_to_be_connected: List[int]
-        if virtual_site == self.__top_virtual_site:
-            sites_to_be_connected = [
-                site for site in range(0, self.__n)
-            ]
-        else:
-            sites_to_be_connected = [
-                site for site in range(self.__n * (self.__n - 1), self.__total_normal_sites)
-            ]
-        for site in sites_to_be_connected:
-            self.__union_find.union(virtual_site, site)
-
