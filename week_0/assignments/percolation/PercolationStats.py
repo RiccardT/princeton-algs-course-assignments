216a0e494e767a41d3a697459f0019b4fd0ea8ae
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-12-20 16:02:09 -0500

    Finished Assignment
    Finished the assignment for week_0! Finally <3

--- week_0/assignments/percolation/PercolationStats.py
+++ week_0/assignments/percolation/PercolationStats.py
@@ -1,3 +1,7 @@
+import random
+from math import sqrt
+from typing import List, Set, Tuple
+
 from week_0.assignments.percolation.interfaces.PercolationStatsInterface import PercolationStatsInterface
 from week_0.assignments.percolation.Percolation import Percolation
 
@@ -5,17 +9,47 @@ from week_0.assignments.percolation.Percolation import Percolation
 class PercolationStats(PercolationStatsInterface):
 
     def __init__(self, n: int, trials: int):
-        self.percolation: Percolation = Percolation(n)
-        self.trials: int = trials
+        self.__n: int = n
+        self.__trials: int = trials
+        self.__p_experiment = None
+        self.__total_number_of_sites: int = n ** 2
+        self.__p_experiment_thresholds: List[float] = []
+        self.__run_trials()
+
+    def __run_trials(self):  # O(T*n^2*log(n))
+        for _ in range(self.__trials):
+            self.__perform_monte_carlo_percolation_experiment()
+
+    def __perform_monte_carlo_percolation_experiment(self):  # O(n^2*log(n))
+        self.__p_experiment = Percolation(self.__n)
+        while not self.__p_experiment.percolates():  # O(n^2*log(n))
+            random_row: int = random.randrange(0, self.__n)
+            random_col: int = random.randrange(0, self.__n)
+            if not self.__p_experiment.is_open(random_row, random_col):
+                self.__p_experiment.open(random_row, random_col)  # O(log(n))
+
+        percolation_threshold: float = self.__p_experiment.number_of_open_sites() / self.__total_number_of_sites
+        self.__p_experiment_thresholds.append(percolation_threshold)
+        self.__p_experiment = None
 
     def mean(self) -> float:
-        return 0.0
+        return sum(self.__p_experiment_thresholds) / self.__trials
 
     def standard_deviation(self) -> float:
-        return 0.0
+        sample_mean: float = self.mean()
+        sum_of_differences_squared: float = 0.0
+        for threshold in self.__p_experiment_thresholds:
+            sum_of_differences_squared += (threshold - sample_mean) ** 2
+        variance: float = sum_of_differences_squared / (self.__trials - 1)
+        sample_standard_deviation: float = sqrt(variance)
+        return sample_standard_deviation
 
     def confidence_low(self) -> float:
-        pass
+        sample_mean: float = self.mean()
+        sample_standard_deviation: float = self.standard_deviation()
+        return sample_mean - (1.96 * sqrt(sample_standard_deviation)) / sqrt(self.__trials)
 
     def confidence_high(self) -> float:
-        pass
+        sample_mean: float = self.mean()
+        sample_standard_deviation: float = self.standard_deviation()
+        return sample_mean + (1.96 * sqrt(sample_standard_deviation)) / sqrt(self.__trials)
---
+++ week_0/assignments/percolation/test_PercolationStats.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+from week_0.assignments.percolation.PercolationStats import PercolationStats
+
+
+class TestPercolationStats(TestCase):
+
+    def test_mean(self):
+        p_stats: PercolationStats = PercolationStats(2, 10000)
+        expected: float = 0.66
+        actual: float = p_stats.mean()
+        self.assertAlmostEqual(expected, actual, delta=0.1)
+
+    def test_standard_deviation(self):
+        p_stats: PercolationStats = PercolationStats(2, 10000)
+        expected: float = 0.1177
+        actual: float = p_stats.standard_deviation()
+        self.assertAlmostEqual(expected, actual, delta=0.1)
+
+    def test_confidence_low(self):
+        p_stats: PercolationStats = PercolationStats(2, 10000)
+        expected: float = 0.66
+        actual: float = p_stats.confidence_low()
+        self.assertAlmostEqual(expected, actual, delta=0.1)
+
+    def test_confidence_high(self):
+        p_stats: PercolationStats = PercolationStats(2, 10000)
+        expected: float = 0.66
+        actual: float = p_stats.confidence_high()
+        self.assertAlmostEqual(expected, actual, delta=0.1)
