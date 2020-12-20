432efae0d5ccb5237918a230e843f51acc79efe0
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-12-20 11:08:34 -0500

    Implemented Percolation + Test Suite
    Got Percolation implemented and tested, now on to the P Stat

--- week_0/assignments/percolation/Percolation.py
+++ week_0/assignments/percolation/Percolation.py
@@ -15,33 +15,46 @@ class Percolation(PercolationInterface):
         self.__bottom_virtual_site: int = self.__total_normal_sites + 1
         self.__connect_virtual_site(self.__top_virtual_site)
         self.__connect_virtual_site(self.__bottom_virtual_site)
-        self.__opened: List[List[bool]] = [
-            [False for _ in range(n)] for _ in range(n)
-        ]
+        self.__opened: List[bool] = [False for _ in range(self.__total_normal_sites)]
+        self.__number_of_open_sites: int = 0
 
     def open(self, row: int, col: int):
-        self.__opened[col][row] = True
         u_f_node: int = self.__get_associated_u_f_node(row, col)
-        adjacent_u_f_nodes: List[int] = self.__get_adjacent_u_f_nodes(row, col)
-        # TODO: Figure out how to fully go from the union find node paradigm to the percolation matrix paradigm and back
+        self.__opened[u_f_node] = True
+        neighbor_u_f_nodes: List[int] = self.__get_neighbor_u_f_nodes(u_f_node)
+        for neighbor_node in neighbor_u_f_nodes:
+            if self.__opened[neighbor_node]:
+                self.__union_find.union(u_f_node, neighbor_node)
+        self.__number_of_open_sites += 1
 
     def __get_associated_u_f_node(self, row: int, col: int) -> int:
         return row * self.__n + col
 
-    def __get_adjacent_u_f_nodes(self, row: int, col: int) -> List[int]:
-        pass
+    def __get_neighbor_u_f_nodes(self, node: int) -> List[int]:
+        left: int = node - 1
+        right: int = node + 1
+        up: int = node - self.__n
+        down: int = node + self.__n
+        potential_neighbors: List[int] = [left, right, up, down]
+        actual_neighbors: List[int] = []
+        for potential_neighbor in potential_neighbors:
+            if potential_neighbor in range(self.__total_normal_sites):
+                actual_neighbors.append(potential_neighbor)
+        return actual_neighbors
 
     def is_open(self, row: int, col: int) -> bool:
-        return False
+        u_f_node: int = self.__get_associated_u_f_node(row, col)
+        return self.__opened[u_f_node]
 
     def is_full(self, row: int, col: int) -> bool:
-        return False
+        u_f_node: int = self.__get_associated_u_f_node(row, col)
+        return self.__opened[u_f_node] and self.__union_find.connected(self.__top_virtual_site, u_f_node)
 
     def number_of_open_sites(self) -> int:
-        return 0
+        return self.__number_of_open_sites
 
     def percolates(self) -> bool:
-        return False
+        return self.__union_find.connected(self.__top_virtual_site, self.__bottom_virtual_site)
 
     def __connect_virtual_site(self, virtual_site: int):  # O(n*log(n))
         sites_to_be_connected: List[int]
--- week_0/assignments/percolation/test_Percolation.py
+++ week_0/assignments/percolation/test_Percolation.py
@@ -5,24 +5,60 @@ from week_0.assignments.percolation.Percolation import Percolation
 
 class TestPercolation(TestCase):
 
-    def test_that_all_sites_are_created(self):
-        n: int = 4
-        percolation: Percolation = Percolation(n)
-        expected: int = n**2 + 2
-        actual: int = len(percolation.__union_find.roots)
+    def setUp(self) -> None:
+        self.n: int = 3
+        self.percolation: Percolation = Percolation(self.n)
+
+    def tearDown(self) -> None:
+        self.n = None
+        self.percolation = None
+
+    def test_open_middle_node(self):
+        self.percolation.open(1, 1)
+        self.assertTrue(self.percolation.is_open(1, 1))
+
+    def test_open_edge_node(self):
+        self.percolation.open(0, 2)
+        self.assertTrue(self.percolation.is_open(0, 2))
+
+    def test_is_open_equals_False(self):
+        self.assertFalse(self.percolation.is_open(1, 1))
+
+    def test_is_open_equals_True(self):
+        self.percolation.open(1, 1)
+        self.assertTrue(self.percolation.is_open(1, 1))
+
+    def test_is_full_equals_False_in_top_row(self):
+        self.assertFalse(self.percolation.is_full(0, 0))
+
+    def test_is_full_equals_False_in_general_case(self):
+        self.assertFalse(self.percolation.is_full(1, 2))
+
+    def test_is_full_equals_True(self):
+        self.percolation.open(0, 0)
+        self.assertTrue(self.percolation.is_full(0, 0))
+
+    def test_number_of_open_sites_on_initial_state(self):
+        expected: int = 0
+        actual: int = self.percolation.number_of_open_sites()
         self.assertEqual(expected, actual)
 
-    def test_open(self):
-        self.fail()
+    def test_number_of_open_sites_in_general_case(self):
+        self.percolation.open(1, 1)
+        expected: int = 1
+        actual: int = self.percolation.number_of_open_sites()
+        self.assertEqual(expected, actual)
 
-    def test_is_open(self):
-        self.fail()
+    def test_percolates_is_False_on_initial_state(self):
+        self.assertFalse(self.percolation.percolates())
 
-    def test_is_full(self):
-        self.fail()
+    def test_percolates_is_False_in_general_case(self):
+        self.percolation.open(0, 0)
+        self.percolation.open(1, 2)
+        self.assertFalse(self.percolation.percolates())
 
-    def test_number_of_open_sites(self):
-        self.fail()
-
-    def test_percolates(self):
-        self.fail()
+    def test_percolates_is_True(self):
+        self.percolation.open(0, 1)
+        self.percolation.open(1, 1)
+        self.percolation.open(2, 1)
+        self.assertTrue(self.percolation.percolates())
